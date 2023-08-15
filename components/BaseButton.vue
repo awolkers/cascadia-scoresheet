04f989b2c6a2ba39fc76fb216c3adb865898cbf6
A. Wolkers <arno.wolkers@gmail.com>
2023-08-15 17:53:38 +0200

    initital setup BaseButton

---
+++ components/BaseButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+interface Props {
+  to?: string;
+  label: string;
+  type?: 'submit' | 'button' | 'reset';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  to: undefined,
+  type: 'button',
+});
+
+const tag = computed(() => {
+  if (props.to) {
+    return 'NuxtLinkLocale';
+  }
+
+  return 'button';
+});
+</script>
+
+<template>
+  <component :is="tag" :class="$style.button" :type="type">{{ label }}</component>
+</template>
+
+<style module>
+.button {
+  border: 0;
+  background: var(--color-primary);
+  color: var(--color-white);
+  font-weight: var(--font-weight-bold);
+  border-radius: var(--border-radius-small);
+  padding: var(--space-8);
+  cursor: pointer;
+}
+</style>
