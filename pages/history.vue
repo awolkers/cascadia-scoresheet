07dad295cc6578553c8ea30edddb985493ea7258
A. Wolkers <arno.wolkers@gmail.com>
2023-09-05 14:41:32 +0200

    updated todos

--- pages/history.vue
+++ pages/history.vue
@@ -7,10 +7,20 @@ useSeoMeta({
 <template>
   <div>
     <h1>History</h1>
-    <p>Todo:</p>
+    <p>Todo history:</p>
     <ul>
       <li>Summary of wins per player</li>
       <li>List of history games</li>
+      <li>History game details</li>
+      <li>History add end date to game</li>
+    </ul>
+    <p>Todo homepage:</p>
+    <ul>
+      <li>Add player name</li>
+      <li>style score sheet</li>
+      <li>style new game form</li>
+      <li>Add best of x games</li>
+      <li>a11y label for input fields</li>
     </ul>
   </div>
 </template>
