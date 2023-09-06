acefa65cc7f52c081dc991b0c1c9de47a817283a
A. Wolkers <arno.wolkers@gmail.com>
2023-09-06 12:19:29 +0200

    todo list cleanup

--- pages/history.vue
+++ pages/history.vue
@@ -15,14 +15,10 @@ useSeoMeta({
     </template>
     <p>Todo history:</p>
     <ul>
-      <li>Summary of wins per player</li>
-      <li>List of history games</li>
-      <li>History game details</li>
       <li>History add end date to game</li>
     </ul>
     <p>Todo homepage:</p>
     <ul>
-      <li>Add player name</li>
       <li>style score sheet</li>
       <li>style new game form</li>
       <li>Add best of x games</li>
