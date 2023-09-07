be2cb17074440739c54f3238c28d2f30dc47a30d
A. Wolkers <arno.wolkers@gmail.com>
2023-09-07 12:34:04 +0200

    added reset button to header

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,7 +1,46 @@
+<script setup lang="ts">
+const store = useGameStore();
+
+const [isDialogOpen, toggleDialog] = useToggle(false);
+
+const onResetHandler = () => {
+  if (!store.players.length) {
+    navigateTo('/');
+  } else {
+    toggleDialog(true);
+  }
+};
+
+const resetScoreSheet = () => {
+  store.initScoreSheet();
+  toggleDialog();
+  navigateTo('/');
+};
+
+const resetGame = () => {
+  store.$reset();
+  toggleDialog();
+  navigateTo('/');
+};
+</script>
+
 <template>
   <header :class="$style['page-header']">
     <h1>{{ $t('site.title') }}</h1>
     <TheNavigation />
+
+    <BaseButton label="Reset" @click="onResetHandler" />
+    <BaseDialog :open="isDialogOpen">
+      <h3>What do you want to reset?</h3>
+      <p>
+        You can reset the current score sheet or quit the current session. When you quit, all game data including the
+        history will be lost.
+      </p>
+      <BaseButtonGroup>
+        <BaseButton label="Reset score sheet" @click="resetScoreSheet" />
+        <BaseButton label="Quit session" secondary @click="resetGame" />
+      </BaseButtonGroup>
+    </BaseDialog>
   </header>
 </template>
 
