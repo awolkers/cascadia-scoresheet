9a0765f54b23bbd412f7aa7de1c492fc4ae38baf
A. Wolkers <arno.wolkers@gmail.com>
2023-09-07 16:47:27 +0200

    added todo to navigation

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -10,9 +10,12 @@ const store = useGameStore();
       </li>
       <li>
         <NuxtLinkLocale to="/history">
-          Game history
+          History
           <span v-if="store.gamesInHistory">({{ store.gamesInHistory }})</span>
         </NuxtLinkLocale>
+      </li>
+      <li>
+        <NuxtLinkLocale to="/todos">Todo's</NuxtLinkLocale>
       </li>
     </ul>
   </nav>
@@ -22,7 +25,7 @@ const store = useGameStore();
 .the-navigation {
   display: flex;
   padding: 0;
-  margin: var(--space-8);
+  /* margin: var(--space-8); */
   list-style: none;
   gap: var(--space-8);
 }
